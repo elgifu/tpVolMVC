87ead9b8fa476ea2d278ed8c3f4ca8eb0d16a3bd
Florent Hastoy <florent.hastoy@gmail.Com>
2018-08-22 15:18:20 +0200

    modification edit.jsp et ajout reservation.jsp pour la classe client

--- tpVolMVC/ProjetDestinationFinale/src/main/java/repositories/ClientRepository.java
+++ tpVolMVC/ProjetDestinationFinale/src/main/java/repositories/ClientRepository.java
@@ -1,5 +1,6 @@
 package repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,10 +8,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import model.Client;
+import model.Reservation;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
 	@Query("select c from Client c left join fetch c.reservations where c.id_client=:idClient")
-	Optional<Client> findCustomByIdWithReservation(@Param("idClient") Long id_client);
+	List<Client> findCustomByIdWithReservation(@Param("idClient") Long id_client);
 
 }
