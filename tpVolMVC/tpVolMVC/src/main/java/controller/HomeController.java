3921d8dd6f572cb4b576613878602bb5850d6027
Thomas <thomas.bottoni@outlook.com>
2018-08-22 11:31:28 +0200

    HomeController + home.jsp

--- tpVolMVC/tpVolMVC/src/main/java/controller/HomeController.java
+++ tpVolMVC/tpVolMVC/src/main/java/controller/HomeController.java
@@ -1,5 +1,16 @@
 package controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/home")
 public class HomeController {
+
+	@RequestMapping("")
+	public ModelAndView home() {
+		return new ModelAndView("redirect:/home/");
+	}
 
 }
